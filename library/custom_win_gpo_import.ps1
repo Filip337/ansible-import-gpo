f8361004c1924e22fca330d28825f73b37b76770
Filip Windows <Filip@lab.local>
2026-06-14 17:51:20 +0200

    ps1 modified

--- library/custom_win_gpo_import.ps1
+++ library/custom_win_gpo_import.ps1
@@ -3,9 +3,9 @@
 
 $spec = @{
     options = @{
-        domain   = @{type="str"; required=$true}
+        domain      = @{type="str"; required=$true}
         backup_path = @{type="str"; required=$true}
-        gpo_nam  = @{type="str"; required=$false; default=$null}
+        gpo_nam     = @{type="str"; required=$false; default=$null}
     }
 }
 
@@ -19,3 +19,34 @@ $domain     = $module.Params.domain
 
 
 #logic
+#get all available ps module on server
+$ModuleInstalled = Get-Module -ListAvailable  
+
+if ($ModuleInstalled.Name -contains "ActiveDirectory") {  
+
+    #if AD module present, import GP module
+    Import-Module GroupPolicy
+    
+    #check if GPO exist with specified name
+    $existingGPO = Get-GPO -Name $gpoName -Domain $domain -ErrorAction SilentlyContinue
+
+    if ($existingGPO) {
+        $module.Result.changed = $false
+        $module.Result.gpo_id  = $existingGPO.Id.ToString()
+        $module.Result.msg     = "GPO '$gpoName' already exists"
+        $module.ExitJson()
+    }
+
+    $importedGPO = Import-GPO -BackupGpoName $gpoName -Path $backupPath -TargetName $gpoName -CreateIfNeeded -Domain $domain
+
+    $module.Result.changed = $true
+    $module.Result.gpo_id  = $importedGPO.Id.ToString()
+    $module.Result.msg     = "GPO '$gpoName' imported with succes"
+    $module.ExitJson()
+
+} else {
+    
+    $module.Result.changed = $false
+    $module.Result.msg     = "ActiveDirectory module not installed on this host"
+    $module.ExitJson()
+}
