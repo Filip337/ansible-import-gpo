23f0178f00079a978623b32e08aa75a03c65cd23
Filip Windows <Filip@lab.local>
2026-06-14 16:08:16 +0200

    Inventori and module file modified

--- library/custom_win_gpo_import.ps1
+++ library/custom_win_gpo_import.ps1
@@ -1,1 +1,18 @@
 #!powershell
+#AnsibleRequires -CSharpUtil Ansible.Basic
+
+$spec = @{
+    options = @{
+        domain   = @{type="str"; required=$true}
+        bkp_path = @{type="str"; required=$true}
+        gpo_nam  = @{type="str"; required=$false; default=$null}
+    }
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args; $spec)
+
+#required in playbook
+$backupPath = $module.Params.backup_path
+$gpoName    = $module.Params.gpo_name
+#optional in playbook
+$domain     = $module.Params.domain
